ac6098a0bbd775047a53b05ba61363aaeefe9da4
Amir Eliz <amir.eliz155@gmail.com>
2023-01-24 00:02:43 +0200

    bte israel

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Countdown from "react-countdown";
+import { useState } from "react";
+
+const Select = ({ lang, setLang }) => {
+  return (
+    <div className="dropdown" style={{ marginTop: 40 }}>
+      <button className="dropbtn">{lang} ▼</button>
+      <div className="dropdown-content">
+        <a href="#" onClick={() => setLang("English")}>
+          English
+        </a>
+        <a href="#" onClick={() => setLang("German")}>
+          German
+        </a>
+        <a href="#" onClick={() => setLang("Hebrew")}>
+          Hebrew
+        </a>
+      </div>
+    </div>
+  );
+};
 
 function App() {
+  const [lang, setLang] = useState("English");
+
+  const date = new Date();
+  const month = date.getUTCMonth() + 1; //months from 1-12
+  const day = date.getUTCDate();
+  console.log(date, month, day);
+
+  const strings = {
+    English: `In ${
+      27 - 1 - day
+    } days (27.1) the International Holocaust Museum in Minecraft will be open for all.`,
+    German: `In ${
+      27 - 1 - day
+    } Tagen (27.1) wird das Internationale Holocaust-Museum in Minecraft für alle geöffnet sein.`,
+    Hebrew: `בעוד ${
+      27 - 1 - day
+    } ימים (27.1) מוזיאון השואה הבינלאומי במיינקראפט יפתח עבור כולם.`,
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {strings[lang]}
+        <Select lang={lang} setLang={setLang} />
       </header>
     </div>
   );
